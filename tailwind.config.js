0821921ebc07aad928f6b2f83c0f05ba82668052
Tomas luquez <luquez1431@gmail.com>
2023-12-20 13:06:05 -0300

    cambie nombres colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         verdeCheck: '#506F20',
         bgNav: '#E8EEE0',
         bgIcons: '#EEF4F8',
-        verdeClaro: '#e8eee0',
+        bgVerdeClaro: '#e8eee0',
         borderColor: '#9994',
         verdeSubtitles: '#D9E4C9',
         stars: '#fdb900',
