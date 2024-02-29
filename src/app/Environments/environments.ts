01808ab3ea5b78a3c7a0443e239e8ba3472ed1f6
Spawnadmin <143339377+Spawnadmin@users.noreply.github.com>
2024-02-29 16:16:12 -0300

    actualizo url db

--- src/app/Environments/environments.ts
+++ src/app/Environments/environments.ts
@@ -3,7 +3,7 @@ export const environment = {
   // API_LOCATION_KEY: '&key=41ff07272c1e436490228a8d825a49cc',
   // API_LOCATION_URL: 'https://api.opencagedata.com/geocode/v1/json?q=',
   API_LOCATION_URL: 'https://api64.ipify.org?format=json',
-  API_DB_URL: 'http://localhost:5050',
+  API_DB_URL: 'https://prod-atlas.azurewebsites.net',
   URL_PLANS: '/api/plans',
   URL_DISCOUNTS: '/api/discounts',
   URL_SETTINGS: '/api/settings',
