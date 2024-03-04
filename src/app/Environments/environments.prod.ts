5c7407c3d739c6f03f3f7df019fc7796363be98b
Tomas luquez <luquez1431@gmail.com>
2024-03-04 11:57:55 -0300

    cambie url api db

--- src/app/Environments/environments.prod.ts
+++ src/app/Environments/environments.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   // API_LOCATION_URL: 'https://api.opencagedata.com/geocode/v1/json?q=',
   API_LOCATION_URL: 'https://api.ipdata.co?api-key=',
   API_KEY_LOCATION: 'b8097bf8d138ee0ad837a8859c97e39bbc3c28f75fc40e82309c98e8',
-  API_DB_URL: 'https://prod-atlas.azurewebsites.net',
+  API_DB_URL: 'https://prod-atlas.nucleocheck.com/',
   URL_PLANS: '/api/plans',
   URL_DISCOUNTS: '/api/discounts',
   URL_SETTINGS: '/api/settings',
